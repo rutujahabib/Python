a44b987cac9efb23f8633f0f3b7f5e1462a54879
rutujahabib <104141040+rutujahabib@users.noreply.github.com>
2025-04-18 01:09:26 +0530

    Add files via upload

---
+++ Day 2/main.py
@@ -0,0 +1,9 @@
+
+
+two_digit_number=input("type a two digit number:")
+first_digit=two_digit_number[0]
+second_digit=two_digit_number[1]
+total = int(first_digit) + int(second_digit)
+print(total)
+
+
